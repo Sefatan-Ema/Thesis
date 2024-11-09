c3410de62bce2878e40d633f9ac9243c396ff916
Sefatan-Sowda-Ema <58888109+Sefatan-Ema@users.noreply.github.com>
2024-11-09 19:35:17 +0600

    FAIA.m

--- FAIA.m
+++ FAIA.m
@@ -1,13 +1,14 @@
 DatawithOri = xlsread('D:\Thesis\data\EEG_Eye_State_Classification.csv');
 %DatawithMain = xlsread('D:\Thesis\data\5%_EEG_Eye_State_Classification.csv');
 %DatawithMain = xlsread('D:\Thesis\data\10%_EEG_Eye_State_Classification.csv');
-%DatawithMain = xlsread('D:\Thesis\data\15%_EEG_Eye_State_Classification.csv');
-DatawithMain = xlsread('D:\Thesis\data\20%_EEG_Eye_State_Classification.csv');
+DatawithMain = xlsread('D:\Thesis\data\15%_EEG_Eye_State_Classification.csv');
+%DatawithMain = xlsread('D:\Thesis\data\20%_EEG_Eye_State_Classification.csv');
 origin=DatawithOri(1:2000, :);
 DatawithMain=DatawithMain(1:2000, :);
 
 %Only Missing............................................................
 %FID_data=ProposedMethod(DatawithMain);
+%FID_data=ProposedMethod2(DatawithMain);
 %FID_data=coefficient_IDIM_recovery(DatawithMain);
 %Only called by datastream2.......
 %FID_data=Proposed2(DatawithMain);
@@ -18,9 +19,9 @@ DatawithMain=DatawithMain(1:2000, :);
 %..............................................................................
 
 %missing with imbalence.......................................................
-%FID_data=Proposed2_TFS(DatawithMain);%proposed mathod call kora
+FID_data=Proposed2_TFS(DatawithMain);%proposed mathod call kora
 
-FID_data=FID_TFS_orginal(DatawithMain);
+%FID_data=FID_TFS_orginal(DatawithMain);
 %..............................................................................
 
 
